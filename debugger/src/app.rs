7c417720c573376faba2e131e472168c8a5e3370
Iamhere <95515863+Iamhere345@users.noreply.github.com>
2024-04-30 11:38:45 +1000

    make debugging emulator timing framerate independent

--- debugger/src/app.rs
+++ debugger/src/app.rs
@@ -1,11 +1,16 @@
+use std::time::{Duration, Instant};
+
 use eframe::{egui, App};
 
 use emu::Gameboy;
 
 use crate::components::{control::Control, cpu::Cpu};
 
+const CYCLES_PER_FRAME: usize = (4194304.0 / 60.0) as usize;
+
 pub struct Debugger {
 	emu: Gameboy,
+	last_update: Instant,
 
 	control: Control,
 	cpu: Cpu,
@@ -15,10 +20,11 @@ impl Debugger {
 	pub fn new(cc: &eframe::CreationContext) -> Self {
 
 		let mut emu = Gameboy::new();
-		emu.init(include_bytes!("../../tests/instr_timing/instr_timing.gb"));
+		emu.init(include_bytes!("../../tests/cpu_instrs/cpu_instrs.gb"));
 
 		Self {
 			emu: emu,
+			last_update: Instant::now(),
 
 			control: Control::new(),
 			cpu: Cpu::new(),
@@ -28,6 +34,22 @@ impl Debugger {
 
 impl App for Debugger {
 	fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+
+		if self.last_update.elapsed() >= Duration::from_secs_f64(1.0 / 60.0) && !self.control.paused {
+			let mut frames = self.last_update.elapsed().as_secs_f64();
+
+			while frames >= 1.0 / 60.0 {
+				for i in 0..CYCLES_PER_FRAME {
+					for _ in 0..self.control.speed {
+						self.emu.tick();
+					}
+				}
+
+				frames -= CYCLES_PER_FRAME as f64;
+			}
+
+			self.last_update = Instant::now();
+		}
 
 		egui::SidePanel::left("left_pannel").show(ctx, |ui| {
 
@@ -49,14 +71,6 @@ impl App for Debugger {
 		egui::CentralPanel::default().show(ctx, |ui| {
 			ui.label("Hello, World!");
 		});
-		
-		// run an instruction every update (which is every vsync, so it depends on the monitor refresh rate)
-		// this temporary; eventually this will be PPU or APU synced
-		if !self.control.paused {
-			for _ in 0..self.control.speed {
-				self.emu.tick();
-			}
-		}
 		
 		ctx.request_repaint();
 
