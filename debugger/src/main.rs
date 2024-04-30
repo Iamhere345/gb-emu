307aaa3c4cda114c4d8ed98296249a58934e1422
Iamhere <95515863+Iamhere345@users.noreply.github.com>
2024-04-30 11:38:45 +1000

    make debugging emulator timing framerate independent

--- debugger/src/main.rs
+++ debugger/src/main.rs
@@ -9,19 +9,6 @@ mod components;
 fn main() {
 	let native_options = NativeOptions::default();
 
-    //run_native("GB Emulator", native_options, Box::new(|cc| Box::new(Debugger::new(cc)))).expect("Unable to initialise egui app");
+    run_native("GB Emulator", native_options, Box::new(|cc| Box::new(Debugger::new(cc)))).expect("Unable to initialise egui app");
 
-	let mut emu = emu::Gameboy::new();
-
-	emu.init(include_bytes!("../../tests/instr_timing/instr_timing.gb"));
-
-	loop {
-		let clock_cycles_per_frame: usize = (4194304.0 / 60.0) as usize;
-
-		for _ in 0..clock_cycles_per_frame {
-			emu.tick();
-		}
-
-		std::thread::sleep(Duration::from_secs_f64(1.0 / 60.0));
-	}
 }
