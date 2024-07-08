66b4a882d96b7d925d246b43a9a79e6c00389221
Iamhere345 <lunarome08@icloud.com>
2024-07-08 16:13:28 +1000

    implement tile rendering code (untested)

--- emu/src/ppu.rs
+++ emu/src/ppu.rs
@@ -1,6 +1,8 @@
 use std::cell::RefCell;
 use std::rc::Rc;
 
+use crate::bus;
+
 use super::interrupt::*;
 
 #[derive(Clone, Copy, PartialEq, Debug)]
@@ -74,6 +76,74 @@ impl LCDC {
 
 }
 
+#[derive(Clone, Copy)]
+enum GbColour {
+	White = 0,
+	LightGrey = 1,
+	DarkGrey = 2,
+	Black = 3
+}
+
+impl From<u8> for GbColour {
+	
+	fn from(from: u8) -> GbColour {
+		match from {
+			0 => GbColour::White,
+			1 => GbColour::LightGrey,
+			2 => GbColour::DarkGrey,
+			3 => GbColour::Black,
+			_ => panic!("invalid EmuColour")
+		}
+	}
+
+}
+
+struct Palette {
+	id_0: GbColour,
+	id_1: GbColour,
+	id_2: GbColour,
+	id_3: GbColour,
+}
+
+impl Palette {
+
+	pub fn new() -> Self {
+		Self {
+			id_0: GbColour::Black,
+			id_1: GbColour::DarkGrey,
+			id_2: GbColour::LightGrey,
+			id_3: GbColour::White,
+		}
+	}
+
+	pub fn get_pal_value(&self, id: u8) -> GbColour {
+
+		match id {
+			0 => self.id_0,
+			1 => self.id_1,
+			2 => self.id_2,
+			3 => self.id_3,
+			_ => panic!("invalid palette index")
+		}
+
+	}
+
+	pub fn read(&self) -> u8 {
+		self.id_0 as u8
+			| self.id_1 as u8 >> 2
+			| self.id_2 as u8 >> 4
+			| self.id_3 as u8 >> 6
+	}
+
+	pub fn write(&mut self, write: u8) {
+		self.id_0 = GbColour::from(write & 0b11);
+		self.id_1 = GbColour::from((write >> 2) & 0b11);
+		self.id_2 = GbColour::from((write >> 4) & 0b11);
+		self.id_3 = GbColour::from((write >> 6) & 0b11);
+	}
+
+}
+
 pub struct PPU {
 	pub rendering_mode: RenderingMode,
 
@@ -88,11 +158,14 @@ pub struct PPU {
 	reg_lyc: u8,        // 0xFF45: if lyc == ly and LYC=LY in STAT register set, raise interrupt
 	reg_stat: u8,		// 0xFF41: LCD status register. Can be used to raise interrupts at various stages of rendering
 	reg_lcdc: LCDC,		// 0xFF40: LCD control register. Can be used to alter the behaviour of the LCD and PPU
+	reg_bgp: Palette,	// 0xFF47: Background palette
 
 	pub line_dots: i32, // amount of dots that has passed; reset each line.
 
 	vram: [u8; 0x2000], // 8k
-	oam: [u8; 160]
+	oam: [u8; 160],
+
+	pixel_buf: [GbColour; 144 * 160]
 }
 
 impl PPU {
@@ -112,11 +185,14 @@ impl PPU {
 			reg_lyc: 0,
 			reg_stat: 0,
 			reg_lcdc: LCDC::new(0),
+			reg_bgp: Palette::new(),
 
 			line_dots: 0,
 
 			vram: [0; 0x2000],
-			oam: [0; 160]
+			oam: [0; 160],
+
+			pixel_buf: [GbColour::Black; 144 * 160]
 		}
 	}
 
@@ -209,11 +285,15 @@ impl PPU {
 
 	}
 
+	/*
+		The gameboy has 32x32 tiles, on a 256x256 pixel grid, however only 160x144 pixels are displayed on the screen.
+	*/
 	fn draw_tiles(&mut self) {
 
 		let draw_window = if self.reg_lcdc.window_enable && self.reg_wy <= self.reg_ly { true } else { false };
 		let mut index_signed = false;
 
+		// the base address of the tile data to be drawn (either 0x8000 or 0x8800)
 		let tile_data: u16 = match self.reg_lcdc.tile_data_area {
 			true => 0x8000,
 			false => {
@@ -223,6 +303,8 @@ impl PPU {
 				0x8800
 			}
 		};
+
+		// base address of the tilemap we're using
 		let tilemap_area: u16 = if draw_window {
 			match self.reg_lcdc.window_tilemap_area {
 				true => 0x9C00,
@@ -235,15 +317,49 @@ impl PPU {
 			}
 		};
 
+		// used to calculate which of the 32 vertical tiles the scanline is on
 		let pos_y: u8 = match draw_window {
 			true => self.reg_ly - self.reg_wy,
 			false => self.reg_scy + self.reg_ly,
 		};
 
-		
-		let tile_row: u16 = ((pos_y / 8) * 32) as u16;
+		// which of the 8 vertical pixels is the scanline on
+		let tile_row: u16 = (pos_y / 8) as u16 * 32;
 
+		for pixel in 0..160 {
 
+			// current x position with scroll / window space conversion
+			let pos_x: u8 = match self.reg_lcdc.window_enable && pixel >= self.reg_wx {
+				false => pixel + self.reg_scx,
+				true => pixel - self.reg_wx
+			};
+
+			// which of the 32 horizontal tiles is pos_x in?
+			let tile_col: u16 = (pos_x / 8) as u16;
+
+			// get the tile id
+			let tile_id = self.read(tilemap_area +tile_row +tile_col);
+
+			let tile_addr = match index_signed {
+				true => tile_data + ((tile_id.wrapping_add(128) as i8) * 16) as u16,
+				false => tile_data + (tile_id * 16) as u16,
+			};
+
+			// the offset from the tile data base for the line of the tile we are on
+			let tile_line = (pos_y % 8) * 2;	// every line is 2 bytes
+
+			let data_1 = self.read(tile_addr + tile_line as u16);
+			let data_2 = self.read(tile_addr + tile_line as u16 + 1);
+
+			// the bit in data 1/2 we are using to get the palette index
+			let colour_bit: i32 = ((pos_x % 8) as i32 - 7) * -1; // idk what this other stuff is doing; TODO test with and without -7*-1
+
+			// combine corresponding bits in data 2/1 to get the bgp index
+			let pal_index = ((data_2 & (1 >> colour_bit)) << 1) | data_1 & (1 >> colour_bit);
+
+			self.pixel_buf[(pixel + 160 * self.reg_ly) as usize] = self.reg_bgp.get_pal_value(pal_index)
+
+		}
 
 	}
 
